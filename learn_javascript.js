4cc7e43ef660ee8c0785d8a631b52bed41e1190e
Salladoretz <salladoretz@yandex.ru>
2022-06-21 19:43:19 +1000

    2.5

--- learn_javascript.js
+++ learn_javascript.js
@@ -1,13 +1,14 @@
 /* Часть 1. Язык JavaScript
 
-2.1
+2.1.1
 <script>
     alert('Я JavaScript!')
 </script>
 
+2.1.2
 <script src="alert.js"></script>
 
-2.4.1.
+2.4.1
 let admin
 let name
 name = 'Джон'
@@ -21,4 +22,10 @@ let currentUser
 2.4.3
 const BIRTHDAY = '18.04.1982'
 const age = someCode(BIRTHDAY)
+
+2.5
+let name = 'Ilya'
+alert( `hello ${1}` ); // hello 1
+alert( `hello ${"name"}` ); // hello name
+alert( `hello ${name}` ); // hello Ilya
 */
