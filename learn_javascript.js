4192187d16941a13cac3f2659def9750cd8339c8
Salladoretz <salladoretz@yandex.ru>
2022-06-22 18:43:21 +1000

    2.15

--- learn_javascript.js
+++ learn_javascript.js
@@ -197,4 +197,65 @@ nextNumber: for (let i = 2; i <= numbersRange; i++) {
     }
     console.log(i)
 }
+
+2.14.1
+if(browser == 'Edge') {
+  alert('You've got the Edge!')
+} else if (browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari'|| browser == 'Opera') {
+  alert('Okay we support these browsers too')
+} else {
+  alert('We hope that this page looks ok!')
+}
+
+2.14.2
+const number = +prompt('Введите число между 0 и 3', '')
+
+switch (number) {
+  case 0:
+    alert('Вы ввели число 0')
+    break
+  case 1:
+    alert('Вы ввели число 1')
+    break
+  case 2:
+  case 3:
+    alert('Вы ввели число 2, а может и 3')
+    break
+}
+
+2.15.1
+нет
+
+2.15.2
+function checkAge(age) {
+  return (age > 18) ? true : confirm('Родители разрешили?')
+}
+function checkAge(age) {
+  return (age > 18) || confirm('Родители разрешили?')
+}
+
+2.15.3
+function min(a, b) {
+  return a < b ? a : b
+}
+
+2.15.4
+function pow(x, n) {
+  let result = x
+
+  for (let i = 1; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+let x = prompt("x?", '')
+let n = prompt("n?", '')
+
+if (n < 1) {
+  alert(`Степень ${n} не поддерживается, используйте натуральное число`)
+} else {
+  alert(pow(x, n))
+}
 */
