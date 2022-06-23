aaecaf99a1f37a19bf0c7d3bfe49ccd10ddb5d72
Salladoretz <salladoretz@yandex.ru>
2022-06-23 16:47:18 +1000

    5.5

--- learn_javascript.js
+++ learn_javascript.js
@@ -487,4 +487,101 @@ function getMaxSubSum(arr) {
   return maxSum
 }
 
+5.5.1
+function camelize(str) {
+  return str
+    .split('-')
+    .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1))
+    .join('')
+}
+
+5.5.2
+function filterRange(arr, a, b) {
+  return arr.filter(item => a <= item && item <= b);
+}
+
+5.5.3
+function filterRangeInPlace(arr, a, b) {
+
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i]
+
+    if (val < a || val > b) {
+      arr.splice(i, 1)
+      i--
+    }
+  }
+}
+
+5.5.4
+arr.sort((a, b) => b - a)
+
+5.5.5
+function copySorted(arr) {
+  return arr.slice().sort()
+}
+
+5.5.6
+function Calculator() {
+
+  this.methods = {
+    "-": (a, b) => a - b,
+    "+": (a, b) => a + b
+  }
+
+  this.calculate = function(str) {
+
+    let split = str.split(' '),
+      a = +split[0],
+      op = split[1],
+      b = +split[2]
+
+    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN
+    }
+
+    return this.methods[op](a, b)
+  }
+
+  this.addMethod = function(name, func) {
+    this.methods[name] = func
+  }
+}
+
+5.5.7
+let names = users.map(item => item.name)
+
+5.5.8
+let usersMapped = users.map(user => ({
+  fullName: `${user.name} ${user.surname}`,
+  id: user.id
+}))
+
+5.5.9
+function sortByAge(arr) {
+  arr.sort((a, b) => a.age > b.age ? 1 : -1)
+}
+
+5.5.10
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5)
+}
+
+5.5.11
+function getAverageAge(users) {
+  return users.reduce((prev, user) => prev + user.age, 0) / users.length
+}
+
+5.5.12
+function unique(arr) {
+  let result = []
+
+  for (let str of arr) {
+    if (!result.includes(str)) {
+      result.push(str)
+    }
+  }
+
+  return result
+}
 */
