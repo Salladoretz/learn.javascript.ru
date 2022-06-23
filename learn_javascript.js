0fafd6f424bde51eec768bde9c702da1dc5871e3
Salladoretz <salladoretz@yandex.ru>
2022-06-23 14:27:22 +1000

    5.3

--- learn_javascript.js
+++ learn_javascript.js
@@ -416,4 +416,49 @@ function randomInteger(min, max) {
   let randInt = min + Math.random() * (max + 1 - min)
   return Math.floor(rand)
 }
+
+5.3.1
+4
+
+5.3.2
+let styles = ['Джаз', 'Блюз']
+
+styled.push('Рок-н-ролл')
+
+styles[Math.floor((styles.length - 1) / 2)] = 'Классика'
+
+alert(styles.shift())
+
+styles.unshift('Рэп', 'Регги')
+
+5.3.4
+function sumInput() {
+
+  let numbers = []
+
+  while (true) {
+    let value = prompt('Введите число', 0)
+    if (value === '' || value === null || !isFinite(value)) break
+    numbers.push(+value)
+  }
+
+  let sum = 0
+  for (let number of numbers) {
+    sum += number
+  }
+  return sum
+}
+
+5.3.5
+function getMaxSubSum(arr) {
+  let maxSum = 0
+  let partialSum = 0
+
+  for (let item of arr) {
+    partialSum += item;
+    maxSum = Math.max(maxSum, partialSum)
+    if (partialSum < 0) partialSum = 0
+  }
+  return maxSum
+}
 */
