b40598e0143f012b1f074448c24196c7cec2cf56
Salladoretz <salladoretz@yandex.ru>
2022-06-25 10:18:48 +1000

    1.11

--- learn_javascript.js
+++ learn_javascript.js
@@ -1184,4 +1184,340 @@ class FormatError extends SyntaxError {
     this.name = 'FormatError'
   }
 }
+
+Часть 2. Браузер: документ, события, интерфейсы
+1.3.1
+document.body.firstElementChild
+document.body.lastElementChild
+document.body.lastElementChild.lastElementChild
+
+1.3.2
+Да
+Нет
+
+1.3.3
+let table = document.body.firstElementChild
+
+for (let i = 0; i < table.rows.length; i++) {
+  let row = table.rows[i]
+  row.cells[i].style.backgroundColor = 'red'
+}
+
+1.4.1
+1. let table = document.getElementById('age-table')
+2. document.querySelectorAll('#age-table label')
+3. table.querySelector('td')
+4. document.querySelector('form[name="search"]')
+5. form.querySelector('input')
+6. let inputs = form.querySelectorAll('input')
+inputs[inputs.length-1]
+
+1.5.1
+for (let li of document.querySelectorAll('li')) {
+  let title = li.firstChild.data
+}
+
+1.5.2
+1
+
+1.5.3
+BODY
+
+1.6.1
+let elem = document.querySelector('[data-widget-name]')
+alert(elem.dataset.widgetName)
+
+1.6.2
+let selector = 'a[href*="://"]:not([href^="http://internal.com"])'
+let links = document.querySelectorAll(selector)
+links.forEach(link => link.style.color = 'orange')
+
+1.7.1
+1
+3
+
+1.7.2
+function clear(elem) {
+  elem.innerHTML = ''
+}
+
+1.7.4
+let ul = document.createElement('ul')
+document.body.append(ul)
+
+while (true) {
+  let data = prompt('Введите текст для элемента списка', '')
+
+  if (!data) {
+    break
+  }
+
+  let li = document.createElement('li')
+  li.textContent = data
+  ul.append(li)
+}
+
+1.7.5
+let data = {
+  'Рыбы': {
+    'форель': {},
+    'лосось': {}
+  },
+  'Деревья': {
+    'Огромные': {
+      'секвойя': {},
+      'дуб': {}
+    },
+    'Цветковые': {
+      'яблоня': {},
+      'магнолия': {}
+    }
+  }
+}
+
+function createTree(container, obj) {
+  container.innerHTML = createTreeText(obj)
+}
+
+function createTreeText(obj) {
+  let li = ''
+  let ul
+  for (let key in obj) {
+    li += '<li>' + key + createTreeText(obj[key]) + '</li>'
+  }
+  if (li) {
+    ul = '<ul>' + li + '</ul>'
+  }
+  return ul || ''
+}
+
+1.7.6
+let lis = document.getElementsByTagName('li')
+
+for (let li of lis) {
+  let descendantsCount = li.getElementsByTagName('li').length
+  if (!descendantsCount) continue
+
+  li.firstChild.data += ' [' + descendantsCount + ']'
+}
+
+1.7.7
+function createCalendar(elem, year, month) {
+  
+  let mon = month - 1
+  let d = new Date(year, mon)
+  let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>'
+  
+  for (let i = 0; i < getDay(d); i++) {
+    table += '<td></td>'
+  }
+  
+  while (d.getMonth() == mon) {
+    table += '<td>' + d.getDate() + '</td>'
+    
+    if (getDay(d) % 7 == 6) {
+      table += '</tr><tr>'
+    }
+    d.setDate(d.getDate() + 1)
+  }
+  
+  if (getDay(d) != 0) {
+    for (let i = getDay(d); i < 7; i++) {
+      table += '<td></td>'
+    }
+  }
+  
+  table += '</tr></table>'
+
+  elem.innerHTML = table
+}
+
+function getDay(date) {
+  let day = date.getDay()
+  if (day == 0) day = 7
+  return day - 1
+}
+
+1.7.8
+let timerId
+
+function update() {
+  let clock = document.getElementById('clock')
+  let date = new Date()
+  let hours = date.getHours()
+  
+  if (hours < 10) hours = '0' + hours
+  clock.children[0].innerHTML = hours
+  
+  let minutes = date.getMinutes()
+  if (minutes < 10) minutes = '0' + minutes
+  clock.children[1].innerHTML = minutes
+  
+  let seconds = date.getSeconds()
+  if (seconds < 10) seconds = '0' + seconds
+  clock.children[2].innerHTML = seconds
+}
+
+function clockStart() {
+  if (!timerId) {
+    timerId = setInterval(update, 1000)
+  }
+  update()
+}
+
+function clockStop() {
+  clearInterval(timerId)
+  timerId = null
+}
+
+1.7.9
+one.insertAdjacentHTML('afterend', '<li>2</li><li>3</li>')
+
+1.7.10
+let sortedRows = Array.from(table.rows)
+  .slice(1)
+  .sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1)
+
+table.tBodies[0].append(...sortedRows)
+
+1.8.1
+function showNotification({top = 0, right = 0, className, html}) {
+  
+  let notification = document.createElement('div')
+  notification.className = 'notification'
+  if (className) {
+    notification.classList.add(className)
+  }
+  
+  notification.style.top = top + 'px'
+  notification.style.right = right + 'px'
+  
+  notification.innerHTML = html
+  document.body.append(notification)
+  
+  setTimeout(() => notification.remove(), 1500)
+}
+
+let i = 1
+setInterval(() => {
+  showNotification({
+    top: 10,
+    right: 10,
+    html: 'Hello ' + i++,
+    className: 'welcome'
+  })
+}
+
+1.10.1
+let scrollBottom = elem.scrollHeight - elem.scrollTop - elem.clientHeight
+
+1.10.2
+let div = document.createElement('div')
+
+div.style.overflowY = 'scroll'
+div.style.width = '50px'
+div.style.height = '50px'
+
+document.body.append(div)
+
+let scrollWidth = div.offsetWidth - div.clientWidth
+
+1.10.3
+ball.style.left = Math.round(field.clientWidth / 2 - ball.offsetWidth / 2) + 'px'
+ball.style.top = Math.round(field.clientHeight / 2 - ball.offsetHeight / 2) + 'px'
+
+1.11.2
+function positionAt(anchor, position, elem) {
+  
+  let anchorCoords = anchor.getBoundingClientRect()
+  
+  switch (position) {
+    case 'top':
+      elem.style.left = anchorCoords.left + 'px'
+      elem.style.top = anchorCoords.top - elem.offsetHeight + 'px'
+      break      
+    case 'right':
+      elem.style.left = anchorCoords.left + anchor.offsetWidth + 'px'
+      elem.style.top = anchorCoords.top + 'px'
+      break
+    case 'bottom':
+      elem.style.left = anchorCoords.left + 'px'
+      elem.style.top = anchorCoords.top + anchor.offsetHeight + 'px'
+      break
+  }
+}
+
+function showNote(anchor, position, html) {
+  let note = document.createElement('div')
+  note.className = 'note'
+  note.innerHTML = html
+  document.body.append(note)
+  
+  positionAt(anchor, position, note)
+}
+
+1.11.3
+function getCoords(elem) {
+  let box = elem.getBoundingClientRect()
+  
+  return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+  }
+}
+
+1.11.4
+
+function getCoords(elem) {
+  let box = elem.getBoundingClientRect()
+
+  return {
+    top: box.top + pageYOffset,
+    left: box.left + pageXOffset
+  }
+}
+
+function showNote(anchor, position, html) {
+
+  let note = document.createElement('div')
+  note.className = 'note'
+  note.innerHTML = html
+  document.body.append(note)
+
+  positionAt(anchor, position, note)
+}
+
+function positionAt(anchor, position, elem) {
+
+  let anchorCoords = getCoords(anchor);
+
+  switch (position) {
+    case 'top-out':
+      elem.style.left = anchorCoords.left + 'px'
+      elem.style.top = anchorCoords.top - elem.offsetHeight + 'px'
+      break
+    case 'right-out':
+      elem.style.left = anchorCoords.left + anchor.offsetWidth + 'px'
+      elem.style.top = anchorCoords.top + 'px'
+      break
+    case 'bottom-out':
+      elem.style.left = anchorCoords.left + 'px'
+      elem.style.top = anchorCoords.top + anchor.offsetHeight + 'px'
+      break
+    case 'top-in':
+      elem.style.left = anchorCoords.left + 'px'
+      elem.style.top = anchorCoords.top + 'px'
+      break
+    case 'right-in':
+      elem.style.width = '150px'
+      elem.style.left = anchorCoords.left + anchor.offsetWidth - elem.offsetWidth + 'px'
+      elem.style.top = anchorCoords.top + 'px'
+      break
+    case 'bottom-in':
+      elem.style.left = anchorCoords.left + 'px'
+      elem.style.top = anchorCoords.top + anchor.offsetHeight - elem.offsetHeight + 'px'
+      break
+  }
+}
+
 */
